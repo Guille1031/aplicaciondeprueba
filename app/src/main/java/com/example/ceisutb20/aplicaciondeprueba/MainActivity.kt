b88cc41dc959635eabef2b867906359ef96bace8
ceisutb20 <ceisutb20@iMac-de-ceisutb20.local>
2018-02-14 14:48:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ceisutb20/aplicaciondeprueba/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ceisutb20.aplicaciondeprueba
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
